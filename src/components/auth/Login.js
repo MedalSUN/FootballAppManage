1ba8e7f6c6c134b412832d875d4cc784a8bea290
MedalSUN <2496667187@qq.com>
2019-05-26 21:15:25 +0800

    更新管理员登录验证mutation方法

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -26,10 +26,8 @@ class Login extends React.Component {
     super(props)
 
     this.state = {
-      email: '',
-      emailError: false,
-      password: '',
-      passwordError: false
+      accountNumber: '',
+      password: ''
     }
   }
 
@@ -45,44 +43,41 @@ class Login extends React.Component {
     console.log('in handle submit')
     this.setState({ ...this.state, token: 'in handle submit' })
     // const { email, password } = this.state
-    const email = 'test@test.test'
+    const accountNumber = '17616023616'
     const password = '123'
-    if (email.length === 0) {
-      return this.setState({ emailError: true })
-    }
-    this.setState({ emailError: false })
+    // if (accountNumber.length === 0) {
+    //   return this.setState({ emailError: true })
+    // }
+    // this.setState({ emailError: false })
 
-    if (password.length === 0) {
-      return this.setState({ passwordError: true })
-    }
-    this.setState({ passwordError: false })
-    console.log('before authenticate email', email)
-    console.log('before authenticate pw', password)
-    console.log('before authenticate', this.props)
+    // if (password.length === 0) {
+    //   return this.setState({ passwordError: true })
+    // }
+    // this.setState({ passwordError: false })
+    console.log('before adminAuthenticate accountNumber', accountNumber)
+    console.log('before adminAuthenticate pw', password)
+    console.log('before adminAuthenticate', this.props)
     this.props
-      .authenticate(email, password)
+      .adminAuthenticate(accountNumber, password)
       .then(({ data }) => {
-        console.log('data', data.authenticate)
+        console.log('data', data.adminAuthenticate)
         console.log('this.props', this.props)
-        this.props.signinUser(data.authenticate.authReturnType.jwt, data.authenticate.authReturnType.id)
-        this.setState({ ...this.state, token: data.authenticate.authReturnType.jwt })
-        return this.props.screenProps.changeLoginState(true, data.authenticate.authReturnType.jwt)
+        this.props.signinUser(data.adminAuthenticate.authReturnType.jwt, data.adminAuthenticate.authReturnType.id)
+        this.setState({ ...this.state, token: data.adminAuthenticate.authReturnType.jwt })
+        return this.props.screenProps.changeLoginState(true, data.adminAuthenticate.authReturnType.jwt)
       })
       .catch(e => {
         console.log('error', e)
-        if (/email/i.test(e.message)) {
-          this.setState({ emailError: true })
-        }
-        if (/password/i.test(e.message)) {
-          this.setState({ passwordError: true })
-        }
+        // if (/email/i.test(e.message)) {
+        //   this.setState({ emailError: true })
+        // }
+        // if (/password/i.test(e.message)) {
+        //   this.setState({ passwordError: true })
+        // }
       })
   };
 
   render () {
-    const { emailError, passwordError } = this.state
-    const { navigation } = this.props
-
     return (
 
       <Container>
@@ -92,12 +87,11 @@ class Login extends React.Component {
 
           <Image style={Styles.topImage} source={require('../../../img/logo.png')} resizeMode="contain"/>
           <Form style={Styles.loginForm}>
-            <Item error={emailError}>
+            <Item>
               <Input
                 fontSize='1'
-                placeholder="请输入邮箱"
-                onChangeText={value => this.handleInputChange('email', value)}
-                keyboardType="email-address"
+                placeholder="请输入管理员账号"
+                onChangeText={value => this.handleInputChange('accountNumber', value)}
                 autoCapitalize="none"
                 autoCorrect={false}
                 allowFontScaling
@@ -110,7 +104,6 @@ class Login extends React.Component {
               itemStyle={Styles.inputError}
               placeholder="请输入密码"
               handleInputChange={this.handleInputChange}
-              passwordError={passwordError}
               label="Password"
               name="password"
               fontSize={Styles.fontSize15}
@@ -118,7 +111,7 @@ class Login extends React.Component {
           </Form>
 
           <View style={Styles.loginButtonView}>
-            <Button onPress={this.handleSubmit} block style={Styles.loginButton} >
+            <Button onPress={this.handleSubmit} block style={Styles.loginButton}>
               <Text style={Styles.loginButtonText} allowFontScaling>登录</Text>
             </Button>
           </View>
@@ -131,17 +124,12 @@ function mapStateToProps (state) {
   return { errorMessage: 'error!' }
 }
 
-// const mapStateToProps = state => ({
-//   selectedRepositoryIds: state.selectedRepositoryIds,
-// });
-
-// export default graphql(
 // eslint-disable-next-line no-class-assign
 Login = graphql(
   gql`
-    mutation authenticate($email:String!,$password:String!) {
-      authenticate(input: {
-        email: $email,
+    mutation adminAuthenticate($accountNumber:String!,$password:String!) {
+      adminAuthenticate(input: {
+        accountNumber: $accountNumber,
         password: $password}) {
         clientMutationId
         authReturnType {
@@ -153,7 +141,7 @@ Login = graphql(
   `,
   {
     props: ({ mutate }) => ({
-      authenticate: (email, password) => mutate({ variables: { email, password } })
+      adminAuthenticate: (accountNumber, password) => mutate({ variables: { accountNumber, password } })
     })
   }
 )(Login)
