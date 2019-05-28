670fe2ba1162dafc305d0836b4a805c6191fc13b
MedalSUN <2496667187@qq.com>
2019-05-28 21:55:24 +0800

    进球管理组件页面

---
+++ src/components/raceDetails/components/GoalDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { Container, Content, Tabs, Tab, View } from 'native-base'
+import { Query } from 'react-apollo'
+
+export default class GoalDetails extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>进球管理</Text>
+      </View>
+    )
+  }
+}
