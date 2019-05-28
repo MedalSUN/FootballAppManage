5ac46cd872f0a24bd67b1368ef579b2237c84ec9
MedalSUN <2496667187@qq.com>
2019-05-28 21:54:28 +0800

    更改IP

--- src/shared_client.js
+++ src/shared_client.js
@@ -2,7 +2,7 @@ import { getToken } from './util'
 import ApolloClient from 'apollo-boost'
 
 export const client = new ApolloClient({
-  uri: 'http://172.20.0.139:5000/graphql',
+  uri: 'http://172.20.1.95:5000/graphql',
   request: async (operation) => {
     const token = await getToken()
     let config = {}
