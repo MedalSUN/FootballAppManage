859a09b4de6137e958c678a8af6c60b971de3abb
MedalSUN <2496667187@qq.com>
2019-06-03 14:55:43 +0800

    进球详情页面

--- src/components/raceDetails/components/GoalDetails.js
+++ src/components/raceDetails/components/GoalDetails.js
@@ -1,15 +1,106 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { Container, Content, Tabs, Tab, View } from 'native-base'
+import { Text, Image, TouchableOpacity } from 'react-native'
+import { Container, Content, View } from 'native-base'
 import { Query } from 'react-apollo'
 import { client } from '../../../shared_client'
+import { ALL_MATCH } from '../../../gql/queries'
+import Styles from './style'
 
 export default class GoalDetails extends React.Component {
   render () {
-    return (
-      <View>
-        <Text>进球管理</Text>
-      </View>
-    )
+    const { navigation } = this.props
+    return <Query query={ALL_MATCH} fetchPolicy='network-only' variables={{ orderBy: 'ORDER_NUMBER_ASC' }}>
+      {({ data, error, loading }) => {
+        if (loading) return <Text>Loading...</Text>
+        if (error) return <Text>`Error! ${error.message}`</Text>
+        return (
+          <View style={[Styles.contentStyle, Styles.contentBox]}>
+            {
+              data.allMatchSchedules.nodes.map((p, i) => {
+                console.log(p.id)
+                return <View key={i}>
+                  {/* 当没有任何进球数据时 */}
+                  {
+                    !p.matchGoalById && <TouchableOpacity style={Styles.matchBox} onPress={() => navigation.navigate('GoalDetailsForm', {
+                      teamAName: p.footballTeamByTeamA.teamName,
+                      teamBName: p.footballTeamByTeamB.teamName,
+                      matchId: p.id,
+                      teamALogo: p.footballTeamByTeamA.imageByTeamLogo.url,
+                      teamBLogo: p.footballTeamByTeamB.imageByTeamLogo.url
+                    })}>
+                      {/* 日期场序轮数 */}
+                      <View style={[Styles.matchBoxTop]}>
+                        <View style={Styles.rowFlex}>
+                          <Text>日期：</Text>
+                          <Text style={Styles.topFont}>{p.matchDate}</Text>
+                        </View>
+                        <View style={Styles.rowFlex}>
+                          <Text>轮数：</Text>
+                          <Text style={Styles.topFont}>{p.wheelNumber}</Text>
+                        </View>
+                        <View style={Styles.rowFlex}>
+                          <Text>场序:</Text>
+                          <Text style={Styles.topFont}>{p.orderNumber}</Text>
+                        </View>
+                      </View>
+                      {/* 主客队 */}
+                      <View style={Styles.matchBoxBottom}>
+                        <View style={Styles.rowFlex}>
+                          <Image style={Styles.imgSize} source={{ uri: p.footballTeamByTeamA.imageByTeamLogo.url }}/>
+                          <Text style={Styles.teamName}>{p.footballTeamByTeamA.teamName}</Text>
+                        </View>
+                        <Text>VS</Text>
+                        <View style={Styles.rowFlex}>
+                          <Text style={Styles.teamName}>{p.footballTeamByTeamB.teamName}</Text>
+                          <Image style={Styles.imgSize} source={{ uri: p.footballTeamByTeamB.imageByTeamLogo.url }}/>
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  }
+                  {/* 存在进球数据，但是没有完成比赛数据录入的 */}
+                  {
+                    p.matchGoalById && p.matchGoalById.finished === false && <TouchableOpacity style={Styles.matchBox} onPress={() => navigation.navigate('GoalDetailsForm', {
+                      teamAName: p.footballTeamByTeamA.teamName,
+                      teamBName: p.footballTeamByTeamB.teamName,
+                      matchId: p.id,
+                      teamALogo: p.footballTeamByTeamA.imageByTeamLogo.url,
+                      teamBLogo: p.footballTeamByTeamB.imageByTeamLogo.url
+                    })}>
+                      {/* 日期场序轮数 */}
+                      <View style={[Styles.matchBoxTop]}>
+                        <View style={Styles.rowFlex}>
+                          <Text>日期：</Text>
+                          <Text style={Styles.topFont}>{p.matchDate}</Text>
+                        </View>
+                        <View style={Styles.rowFlex}>
+                          <Text>轮数：</Text>
+                          <Text style={Styles.topFont}>{p.wheelNumber}</Text>
+                        </View>
+                        <View style={Styles.rowFlex}>
+                          <Text>场序:</Text>
+                          <Text style={Styles.topFont}>{p.orderNumber}</Text>
+                        </View>
+                      </View>
+                      {/* 主客队 */}
+                      <View style={Styles.matchBoxBottom}>
+                        <View style={Styles.rowFlex}>
+                          <Image style={Styles.imgSize} source={{ uri: p.footballTeamByTeamA.imageByTeamLogo.url }}/>
+                          <Text style={Styles.teamName}>{p.footballTeamByTeamA.teamName}</Text>
+                        </View>
+                        <Text>VS</Text>
+                        <View style={Styles.rowFlex}>
+                          <Text style={Styles.teamName}>{p.footballTeamByTeamB.teamName}</Text>
+                          <Image style={Styles.imgSize} source={{ uri: p.footballTeamByTeamB.imageByTeamLogo.url }}/>
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  }
+                </View>
+              })
+            }
+          </View>
+        )
+      }}
+    </Query>
   }
 }
