123463ea3bd8a3e249d1746185e49bf51f2b23d9
MedalSUN <2496667187@qq.com>
2019-06-03 14:55:56 +0800

    增加多个mutation

--- src/gql/mutation.js
+++ src/gql/mutation.js
@@ -11,9 +11,27 @@ mutation changeMatchSchedule($input: ChangeMatchScheduleInput!){
 
 // 用于增加指定比赛的比分
 export const ADD_MATCH_GOALS = gql`
-mutation changeMatchGoal($input: ChangeMatchGoalInput!){
-  changeMatchGoal(input: $input){
-  string
+mutation changeMatchGoalDetails($input:  ChangeMatchGoalDetailsInput!){
+  changeMatchGoalDetails(input: $input){
+    string
+  }
+}
+`
+
+// 用于完成比赛录入
+export const FINISHED_MATCH_GOAL = gql`
+mutation finishedMatchGoal($input: FinishedMatchGoalInput!){
+  finishedMatchGoal(input: $input){
+    string
+  }
+}
+`
+
+// 用于完成加入球队申请
+export const APPROVAL_PERSON_TEAM = gql`
+mutation approval($input: ApprovalInput!){
+  approval(input: $input){
+    string
   }
 }
 `
